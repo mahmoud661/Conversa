5586b6797e143f68a677d02176da2bb97b4dd133
Dooo <63370835+mahmoud661@users.noreply.github.com>
2023-11-12 17:49:56 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,46 +1,14 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-const io = require("socket.io");
-const http = require("http"); // Import the 'http' module
+
 const app = express();
 
-const corsOptions = {
-  origin: ["https://deploy-mern-lwhq.vercel.app"],
-  methods: ["POST", "GET"],
-  optionsSuccessStatus: 200,
-  credentials: true,
-  origin: true,
-};
-
-const saltRounds = 10;
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
-app.use(express.json());
-
-app.get("/api", function (req, res) {
-  res.json({ message: "hello" });
-});
-app.get("/", function (req, res) {
-  res.send("hello");
-  console.log("hello");
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-
-
-
-
-
-
-
-const port = process.env.PORT || 4000; 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
 
+// Export the Express API
 module.exports = app;
